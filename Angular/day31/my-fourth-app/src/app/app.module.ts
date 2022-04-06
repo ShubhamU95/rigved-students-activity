fcd063f3cd06ebf3415071f1fed28786954e19d4
ShubhamU95 <shubhamuthale9@gmail.com>
2022-04-06 09:32:45 +0530

    done

---
+++ Angular/day31/my-fourth-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ActivityComponent } from './activity/activity.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivityComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
